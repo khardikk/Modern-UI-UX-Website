082ac4f1387c5a719c3cf7b1bb19cda406acbf6b
Hardikk <kambojpushkal@gmail.com>
2023-11-26 10:28:26 +0530

    Added Card Deal Component

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button type='button' className={`py-4 px-6 
     bg-blue-gradient font-poppins font-medium text-[18px] 
-    text-primary outline-none mt-10`}>
+    text-primary outline-none mt-10 rounded-[10px]`}>
       Get Started
     </button>
   )
